8e3ab32f0e6e62a9d018e42e19796326a323848a
Howie <howie12207@gmail.com>
2021-08-23 14:44:23 +0800

    feat: add config to build github

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/practice-react-tip-calculator-app/",
   plugins: [reactRefresh(), svgr()],
 });
